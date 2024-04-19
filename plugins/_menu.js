b066edfbfddac3e6e15e4d502343d80c4a44df78
Paulorodi <111430953+Paulorodi@users.noreply.github.com>
2024-04-19 15:17:10 +0300

    Update _menu.js

--- plugins/_menu.js
+++ plugins/_menu.js
@@ -86,7 +86,7 @@ bot.addCommand(
       }
     })
     const date = new Date()
-    let msg = `\`\`\`╭═══ LEVANTER ═══⊷
+    let msg = `\`\`\`╭═══ ORODI-TECH-BOT ═══⊷
 ┃❃╭──────────────
 ┃❃│ Prefix : ${PREFIX}
 ┃❃│ User : ${message.pushName}
